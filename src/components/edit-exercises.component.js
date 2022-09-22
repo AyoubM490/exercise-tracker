28869e690585e5137ebf283857a31c8461c3c033
AyoubMS <chahayb563@gmail.com>
2022-09-22 17:53:55 +0100

    add edit-exercises component tests

--- src/components/edit-exercises.component.js
+++ src/components/edit-exercises.component.js
@@ -71,8 +71,8 @@ const EditExercise = () => {
   };
 
   return (
-    <div>
-      <h3>Edit Exercise Log</h3>
+    <div data-test="component-edit-exercises">
+      <h3 data-test="edit-exercises-title">Edit Exercise Log</h3>
       <form onSubmit={onSubmit}>
         <div className="form-group my-3">
           <label>Username: </label>
@@ -80,6 +80,7 @@ const EditExercise = () => {
             required
             className="form-control"
             value={username}
+            data-test="username-input"
             onChange={onChangeUsername}
           >
             {users.map((user) => (
@@ -96,6 +97,7 @@ const EditExercise = () => {
             required
             className="form-control"
             value={description}
+            data-test="description-input"
             onChange={onChangeDescription}
           />
         </div>
@@ -105,19 +107,25 @@ const EditExercise = () => {
             type="text"
             className="form-control"
             value={duration}
+            data-test="duration-input"
             onChange={onChangeDuration}
           />
         </div>
         <div className="form-group my-3">
           <label>Date: </label>
-          <div>
-            <DatePicker selected={date} onChange={onChangeDate} />
+          <div data-test="date-input">
+            <DatePicker
+              selected={date}
+              onChange={onChangeDate}
+              data-test="date-picker-input"
+            />
           </div>
         </div>
         <div className="form-group my-3">
           <input
             type="submit"
             value="Edit Exercise Log"
+            data-test="submit-button"
             className="btn btn-primary"
           />
         </div>
